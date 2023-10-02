d81ab6d0dd7452bdc6dd5c36e0ebeea4507d30fd
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-10-02 15:37:43 -0400

    Con bucle while

--- src/TCP/servidorTCP.java
+++ src/TCP/servidorTCP.java
@@ -32,5 +32,28 @@ public class servidorTCP {
         //Enviar respuesta
         salida.writeUTF(solicitud);  // Enviar respuesta al cliente
 
+        // ahroa pare múltiples clientes usamos un while
+        while (true) {
+            Socket sock2 = servidor.accept();
+            // esperando a que el cliente se conecte
+            System.out.println("Cliente conectado");
+
+            DataInputStream entrada2 = new DataInputStream(sock2.getInputStream());
+            DataOutputStream salida2 = new DataOutputStream(sock2.getOutputStream());
+
+            //Ingrasar solicitud
+            String solicitud2 = entrada2.readUTF();  // Recibir solicitud del cliente
+            //Covertir a Mayusculas
+            solicitud2 = solicitud2.toUpperCase();
+            System.out.println("Solicitud recibida: " + solicitud2);
+
+            //Enviar respuesta
+            salida2.writeUTF(solicitud2);  // Enviar respuesta al cliente
+        }
+
+        //las difercias entre el código una solicitud y varias es que en el caso de una solicitud
+        //el servidor recibe la solicitud, la procesa y envía la respuesta, y luego se cierra la conexión
+
+
     }
 }
